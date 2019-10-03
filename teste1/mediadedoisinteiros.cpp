2e1060fcbe90085085ff71a0e44052c49b278976
Rafael <eu@rafaelgirao.me>
2019-10-03 09:56:54 +0100

    mais cenas engraçosas

--- teste1/mediadedoisinteiros.cpp
+++ teste1/mediadedoisinteiros.cpp
@@ -6,14 +6,15 @@ using namespace std;
 int main()
 {
     //Banana
-    int base,altura,area;
+    float n1,n2,media;
     setlocale (LC_ALL, "portuguese");
     cout << "Digite o 1º número: ";
     cin >> n1;
     cout << "Digite o 2º número: ";
     cin >> n2;
-    soma=n1+n2;
-    cout << "A soma é: "<<soma<<"\n";
+    media=(n1+n2)/2;
+    cout << "A média é: "<<media<<"\n";
+
 
     return 0;
 }
