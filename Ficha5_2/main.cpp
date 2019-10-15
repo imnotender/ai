2582c0f1496bc558824a74ebcca1ec2bc449840e
Rafael <7544832+imnotender@users.noreply.github.com>
2019-10-15 14:25:53 +0100

    mover ficha 4 p/ pasta própria, continuar resolver ficha 5

--- Ficha5_2/main.cpp
+++ Ficha5_2/main.cpp
@@ -10,10 +10,31 @@ int main()
     //Definir variáveis a usar e locale
     setlocale (LC_ALL, "portuguese");
     int nr;
-    cout << "Digite um número: ";
-    cin >> nr;
-    cout <<s "\nTabuada do nº" << nr<< ": \n";
-    cout
+    bool bad;
+    do
+    {
+        bad = false;
+        cout << "Digite um número: ";
+        cin >> nr;
+        if (!cin.good())
+        {
+            cin.clear();
+            cin.ignore(99999, '\n');
+            cout << "[ERRO] Número inserido é inválido.\n";
+            bad = true;
+        }
+        else
+        {
+            cout << "\nTabuada do nº " << nr<< ": \n";
+            for (int i = 1; i<11; i++)
+            {
+                cout << nr << " * " << i << " = " << nr*i << endl;
+            }
+        }
+
+
+    }
+    while(bad);
 
     return 0;
 }
